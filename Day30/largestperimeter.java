b62bc442fd9a8676e7e39dac488d742b7b0e7fb1
viswa130 <vishwavishwa04743@gmail.com>
2026-07-24 22:21:54 +0530

    Initial

---
+++ Day30/largestperimeter.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class LargestPerimeter {
+    public static int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i >= 2; i--) {
+            if (nums[i - 2] + nums[i - 1] > nums[i]) {
+                return nums[i - 2] + nums[i - 1] + nums[i];
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(largestPerimeter(nums));
+        sc.close();
+    }
+}
