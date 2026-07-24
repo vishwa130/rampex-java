f70d51eaa3c009b772122751de828e121137a91a
viswa130 <vishwavishwa04743@gmail.com>
2026-07-24 22:35:48 +0530

    Initial

---
+++ Day30/program6.java
@@ -0,0 +1,21 @@
+package Day29;
+
+public class Problem6 {
+   
+    public int findJudge(int n, int[][] trust) {
+        int[] score = new int[n + 1];
+
+        for (int[] t : trust) {
+            score[t[0]]--;
+            score[t[1]]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (score[i] == n - 1)
+                return i;
+        }
+
+        return -1;
+    }
+}
+    
