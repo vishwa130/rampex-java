80e2ed8b83976104cb3f76b319202037b1d6f0ce
viswa130 <vishwavishwa04743@gmail.com>
2026-07-24 22:37:02 +0530

    Initial

---
+++ Day30/program7.java
@@ -0,0 +1,29 @@
+package Day29;
+
+public class Problem7 {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int original = image[sr][sc];
+
+        if (original == color)
+            return image;
+
+        dfs(image, sr, sc, original, color);
+
+        return image;
+    }
+
+    private void dfs(int[][] image, int r, int c, int original, int color) {
+        if (r < 0 || c < 0 || r >= image.length || c >= image[0].length)
+            return;
+
+        if (image[r][c] != original)
+            return;
+
+        image[r][c] = color;
+
+        dfs(image, r + 1, c, original, color);
+        dfs(image, r - 1, c, original, color);
+        dfs(image, r, c + 1, original, color);
+        dfs(image, r, c - 1, original, color);
+    }
+}
