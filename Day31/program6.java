78eab90766b3d1bf7e1d58ebf4cb6b546097b013
viswa130 <vishwavishwa04743@gmail.com>
2026-07-27 10:26:47 +0530

    Initial

---
+++ Day31/program6.java
@@ -0,0 +1,18 @@
+class HouseRobber1 {
+
+    public int rob(int[] nums) {
+
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int money : nums) {
+
+            int current = Math.max(prev1, prev2 + money);
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+}
