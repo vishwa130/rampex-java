2e89e934f51b18f6542b40473dd26d41b03fbd32
viswa130 <vishwavishwa04743@gmail.com>
2026-08-07 14:07:03 +0530

    Initial

---
+++ spiralmatrixtraversal/SpiralMatrixTraversal.java
@@ -0,0 +1,50 @@
+package spiralmatrixtraversal;
+
+public import java.util.*;
+
+public class SpiralMatrixTraversal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] mat = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        int top = 0, bottom = m - 1;
+        int left = 0, right = n - 1;
+
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++)
+                System.out.print(mat[top][i] + " ");
+            top++;
+
+            for (int i = top; i <= bottom; i++)
+                System.out.print(mat[i][right] + " ");
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--)
+                    System.out.print(mat[bottom][i] + " ");
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    System.out.print(mat[i][left] + " ");
+                left++;
+            }
+        }
+
+        sc.close();
+    }
+} {
+    
+}
